4227f01130eac0a5e99b84b228058f039ea49e99
KeeganJennings <34640514+KeeganJennings@users.noreply.github.com>
2019-03-11 08:19:21 -0500

    Add project files.

---
+++ Dragon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Dragon
+{
+
+    int horde;
+    int stamina;
+
+	public Dragon()
+	{
+        horde = 0;
+        stamina = 100;
+	}
+
+    public void Raid()
+    {
+        if(stamina - 10 >= 0)
+        {
+            stamina -= 10;
+            horde += 100;
+        }
+    }
+
+    public void Rest()
+    {
+        if(stamina + 10 <= 100)
+        {
+            stamina += 10;
+        }
+    }
+}
